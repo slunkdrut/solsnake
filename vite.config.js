823079291d839140393fef5cde2c45ee85c43d5f
slunkdrut <kaydherriman@gmail.com>
2025-09-11 21:23:20 -0600

    first commit

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: 3000,
-    host: true
+    host: '0.0.0.0',
+    strictPort: false,
+    allowedHosts: [
+      '34e43099d172.ngrok-free.app'
+    ]
   },
   build: {
     outDir: 'dist',
