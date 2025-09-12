05b44c43100c630a775ac46e0cf2f43a3fbf2496
slunkdrut <kaydherriman@gmail.com>
2025-09-11 23:52:11 -0600

    Fix GitHub Pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: '/',
+  base: '/solsnake/',   // 👈 required for GitHub Pages
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
